0c6903ca657b7150028ff3e15be35eab41261270
kkwanyang <k.kwanyang@gmail.com>
2018-04-11 12:24:52 -0400

    database_package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='database_connection',
+      version='0.1',
+      description='A sqlalchemy connection utility',
+      url='http://github.com/kkwanyang/database_connection',
+      author='kkwanyang',
+      author_email='kk.dsdev@gmail.com',
+      license='MIT',
+      packages=['database_connection'],
+      zip_safe=False)
